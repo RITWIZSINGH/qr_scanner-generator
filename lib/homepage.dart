5eff6f42a4eb74f463382ee33f7390a67fdbb80a
Ritwiz <madangopalsingh2803@gmail.com>
2024-11-03 00:07:59 +0530

    Created homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,6 +10,37 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Qr Code Scanner and Generator'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: screenHeight / 5,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text('Scan QR Code'),
+            ),
+            SizedBox(
+              height: screenHeight / 5,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text('Generate QR Code'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
