7a787a3fd9695385ad7232907db0246b1b11de46
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-05 21:45:36 +0530

    Random commit

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class GenerateQrCode extends StatefulWidget {
   const GenerateQrCode({super.key});
@@ -8,8 +9,29 @@ class GenerateQrCode extends StatefulWidget {
 }
 
 class _GenerateQrCodeState extends State<GenerateQrCode> {
+  TextEditingController urlController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Generate Qr code'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              if (urlController.text.isNotEmpty)
+                QrImageView(
+                  data: urlController.text,
+                  size: screenWidth / 4,
+                  version: QrVersions.auto,
+                )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
